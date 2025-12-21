47a8d078c81f326e36fcbf2b8ea190b5a6302e70
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-12-21 04:57:50 +0100

    init

---
+++ 2025/day_01/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs;
+
+fn main() {
+    let input: String = fs::read_to_string("src/input.txt").unwrap();
+    let lines: Vec<&str> = input.lines().collect();
+
+    let mut accum = 50;
+    let mut answer_p1 = 0;
+    let mut answer_p2 = 0;
+
+    for line in &lines {
+        let offset: i32 = line[1..].parse().unwrap();
+        let mut direction: i32 = 1;
+        if line.chars().nth(0).unwrap() == 'L' { direction = -1; }
+
+        for _ in 1..=offset {
+            accum += direction;
+            accum = accum % 100;
+            if accum == 0 { answer_p2 += 1; }
+        }
+
+        if accum == 0 { answer_p1 += 1; }
+    }
+
+    println!("part 1: {}", answer_p1);
+    println!("part 2: {}", answer_p2);
+}
