e070198865d98115cd43a5541dbe2cf9e2e3a2bb
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-12-25 18:33:50 +0100

    fix 2025 day 2 part 2

--- 2025/day_02/src/main.rs
+++ 2025/day_02/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let input: String = String::from("328412-412772,1610-2974,163-270,7693600637-7693779967,352-586,65728-111612,734895-926350,68-130,183511-264058,8181752851-8181892713,32291-63049,6658-12472,720-1326,21836182-21869091,983931-1016370,467936-607122,31-48,6549987-6603447,8282771161-8282886238,7659673-7828029,2-18,7549306131-7549468715,3177-5305,20522-31608,763697750-763835073,5252512393-5252544612,6622957-6731483,9786096-9876355,53488585-53570896");
 
@@ -35,24 +37,22 @@ fn is_invalid_id(id: u64) -> bool {
     &id_str[0..half_len] == &id_str[half_len..id_str_len]
 }
 
+
 fn is_invalid_id_p2(id: u64) -> bool {
     let id_str = id.to_string();
-    let id_str_len = id_str.len();
-    let half_len = id_str_len / 2;
-    // println!("half: {half_len}");
-    let mut invalid = true;
-
-    for n in 1..half_len {
-        // println!("n: {n}");
-        if id_str_len % n != 0 { continue; }
-        let chunks = id_str_len / n;
-        // println!("n: {n}, chunks: {chunks}");
-        let first_chunk = &id_str[0..n];
-        for i in 1..chunks {
-            if id_str[i*n..(i+1)*n] != *first_chunk {
-                invalid = false
-            }
+    let length = id_str.len();
+    for part_count in 2..=length {
+        if length % part_count != 0 { continue; }
+        let part_length = length / part_count;
+        let mut chunks: HashSet<i32> = HashSet::new();
+        for part_id in 0..part_count {
+            let chunk_start = part_id * part_length;
+            let chunk_end = (part_id + 1) * part_length;
+            let chunk = &id_str[chunk_start..chunk_end];
+            chunks.insert(chunk.parse().unwrap());
         }
+        if chunks.len() == 1 { return true; }
     }
-    return invalid
+    return false;
 }
+
